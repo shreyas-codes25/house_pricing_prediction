cdac6a69959161f2fc0db98c7cd8733a4834a991
shreyas-codes25 <140911851+shreyas-codes25@users.noreply.github.com>
2024-11-17 13:03:50 +0530

    wip

--- lib/prediction_result.dart
+++ lib/prediction_result.dart
@@ -74,51 +74,104 @@ class PredictionResultScreen extends StatelessWidget {
                     style: Theme.of(context).textTheme.headlineMedium,
                   ),
                   const SizedBox(height: 8),
-                  Container(
-                    padding: const EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      borderRadius: BorderRadius.circular(8),
-                      boxShadow: [
-                        const BoxShadow(
-                          color: Colors.black26,
-                          blurRadius: 4,
-                          offset: Offset(0, 2),
+                  Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.black26,
+                              blurRadius: 4,
+                              offset: Offset(0, 2),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Loan Amount: ${formatPrice(loanAmount)}"),
-                        Text("Interest Rate: $interestRate%"),
-                        Text("Tenure: $tenure years"),
-                        Text(
-                          "Estimated EMI: $formattedEMI",
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.green,
-                          ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Loan Amount: ${formatPrice(loanAmount)}"),
+                            Text("Interest Rate: $interestRate%"),
+                            Text("Tenure: $tenure years"),
+                            Text(
+                              "Estimated EMI: $formattedEMI",
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.green,
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                      const SizedBox(height: 16),
+                    ],
                   ),
-                  const SizedBox(height: 16),
                 ],
                 Text(
                   "Details:",
                   style: Theme.of(context).textTheme.headlineMedium,
                 ),
-                ...details.entries.map((entry) {
-                  if (entry.key != 'loan_amount' &&
-                      entry.key != 'interest_rate' &&
-                      entry.key != 'tenure') {
-                    return Text("${entry.key}: ${entry.value}");
-                  }
-                  return const SizedBox
-                      .shrink(); // Skip loan-related details in the general details section
-                }),
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.redAccent,
+                        blurRadius: 4,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: details.entries.map((entry) {
+                      if (entry.key != 'loan_amount' &&
+                          entry.key != 'interest_rate' &&
+                          entry.key != 'tenure') {
+                        // Handle boolean keys for "Yes" or "No"
+                        if (entry.key == 'mainroad_yes' ||
+                            entry.key == 'guestroom_yes' ||
+                            entry.key == 'basement_yes' ||
+                            entry.key == 'hotwaterheating_yes' ||
+                            entry.key == 'airconditioning_yes' ||
+                            entry.key == 'prefarea_yes') {
+                          return Text(
+                              "${entry.key}: ${(entry.value == 1) ? "Yes" : "No"}");
+                        }
+
+                        // Handle furnishing status
+                        if (entry.key == 'furnishingstatus_Semi-furnished' &&
+                            entry.value == 1) {
+                          return const Text(
+                              "Furnishing Status: Semi-furnished");
+                        } else if (entry.key ==
+                                'furnishingstatus_Unfurnished' &&
+                            entry.value == 1) {
+                          return const Text("Furnishing Status: Unfurnished");
+                        } else if (entry.key ==
+                                'furnishingstatus_Semi-furnished' &&
+                            entry.value == 0 &&
+                            details['furnishingstatus_Unfurnished'] == 0) {
+                          return const Text("Furnishing Status: Furnished");
+                        }
+
+                        if (entry.key == "furnishingstatus_Semi-furnished" ||
+                            entry.key == "furnishingstatus_Unfurnished") {
+                          return const Text("");
+                        }
+                        // Default case for other keys
+                        return Text("${entry.key}: ${entry.value}");
+                      }
+                      return const SizedBox
+                          .shrink(); // Skip loan-related details in the general details section
+                    }).toList(),
+                  ),
+                )
               ],
             ),
           ),
